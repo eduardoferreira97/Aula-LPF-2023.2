9655c4e9e8bad845822ae7472976e397c9634ed5
Eduardo Ferreira <58628369+eduardoferreira97@users.noreply.github.com>
2023-12-01 08:52:09 -0300

    Atividades de 01/12
    Atividades de 01/12

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,43 @@
+class Lista(val valor:Int, val prox:Lista?){
+
+   override fun toString():String{
+      if(prox != null){
+         return valor.toString() + "->" + prox.toString()
+      }else{
+         return valor.toString()
+      }
+   }
+}
+
+fun fat(x:Int):Int{
+   
+   if(x >= 2){
+      return x * fat(x-1)
+   }else{
+      return x
+   }
+}
+
+fun pot(x:Int, y:Int):Int{
+    if(y > 0){
+        return x * pot(x,y-2)
+    }else{
+        return x
+}
+}
+
+fun fib(x:Int):Int{
+    if(x > 1){
+       
+        return fib(x-1) + fib(x-2)
+    }else{
+        return x
+    }
+}
+
 fun main() {
-   println("ola Kotlin")
+
+   println(fat(5))
+   println(pot(2,2))
+   println(fib(7))
 }
